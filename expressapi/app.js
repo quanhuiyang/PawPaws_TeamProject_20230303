dec92413b4100dc31488441670c12346293dee35
elenalin119 <77560081+elenalin119@users.noreply.github.com>
2023-03-08 15:05:49 +0800

    20230308update

--- expressapi/app.js
+++ expressapi/app.js
@@ -1,60 +1,64 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require('cors')
+
+const dotenv = require('dotenv')
 
 // 1. 引用routes 資料夾下的程式
-const indexRouter = require('./routes/index');
-const shopRouter = require('./routes/shop');
-const hotelRouter = require('./routes/hotel');
-const activityRouter = require('./routes/activity');
-const membersRouter = require('./routes/members');
-const app = express();
-app.use(cors());
+const indexRouter = require('./routes/index')
+const shopRouter = require('./routes/shop')
+const hotelRouter = require('./routes/hotel')
+const activityRouter = require('./routes/activity')
+const membersRouter = require('./routes/members')
+
+const app = express()
+dotenv.config()
+
+app.use(cors())
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // 2. 路由設定
 //http://localhost:3000/
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 
 //http://localhost:3000/shop
-app.use('/shop', shopRouter);
+app.use('/shop', shopRouter)
 
 //http://localhost:3000/hotel
-app.use('/hotel',hotelRouter);
+app.use('/hotel', hotelRouter)
 
 //http://localhost:3000/activity
-app.use('/activity',activityRouter);
+app.use('/activity', activityRouter)
 
 //http://localhost:3000/members
-app.use('/members', membersRouter);
-
+app.use('/members', membersRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
