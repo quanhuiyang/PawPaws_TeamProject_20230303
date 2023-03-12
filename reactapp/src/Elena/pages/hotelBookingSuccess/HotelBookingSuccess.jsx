779592f80918f97f091c9fd44a6409fc2c4aab59
elenalin119 <77560081+elenalin119@users.noreply.github.com>
2023-03-13 01:06:42 +0800

    add booking success page

--- reactapp/src/Elena/pages/hotelBookingSuccess/HotelBookingSuccess.jsx
+++ reactapp/src/Elena/pages/hotelBookingSuccess/HotelBookingSuccess.jsx
@@ -3,7 +3,15 @@ import './hotelBookingSuccess.css'
 import StateBar from '../../../Elena/components/stateBar/StateBar'
 
 function HotelBookingSuccess() {
-  return <>123</>
+  return (
+    <>
+      <StateBar />
+      <div className="bookingSuccessContainer">
+        <h2>恭喜你完成預訂</h2>
+        <h1>訂單代號:HGHT2023032200001</h1>
+      </div>
+    </>
+  )
 }
 
 export default HotelBookingSuccess
