7cf1d0fed4bf17204d72a139322eac30adcc6a8f
elenalin119 <77560081+elenalin119@users.noreply.github.com>
2023-03-13 10:03:01 +0800

    add order detail into hotel booking success page

--- reactapp/src/Elena/pages/hotelBookingSuccess/HotelBookingSuccess.jsx
+++ reactapp/src/Elena/pages/hotelBookingSuccess/HotelBookingSuccess.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './hotelBookingSuccess.css'
 import StateBar from '../../../Elena/components/stateBar/StateBar'
+import HotelBookingHotel from '../../components/hotelBookingHotel/HotelBookingHotel'
 
 function HotelBookingSuccess() {
   return (
@@ -9,6 +10,9 @@ function HotelBookingSuccess() {
       <div className="bookingSuccessContainer">
         <h2>恭喜你完成預訂</h2>
         <h1>訂單代號:HGHT2023032200001</h1>
+        <HotelBookingHotel />
+        <button>查詢訂單</button>
+        <button>回飯店首頁</button>
       </div>
     </>
   )
