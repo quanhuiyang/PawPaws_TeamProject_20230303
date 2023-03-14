1096978f9c1064df4dfdf3da22ec9330ddfc832d
elenalin119 <77560081+elenalin119@users.noreply.github.com>
2023-03-14 20:58:25 +0800

    add btn navigate

--- reactapp/src/Elena/pages/hotelBookingSuccess/HotelBookingSuccess.jsx
+++ reactapp/src/Elena/pages/hotelBookingSuccess/HotelBookingSuccess.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import './hotelBookingSuccess.css'
 import StateBar from '../../../Elena/components/stateBar/StateBar'
 import HotelBookingHotel from '../../components/hotelBookingHotel/HotelBookingHotel'
+import { Link, useNavigate } from 'react-router-dom'
 
 function HotelBookingSuccess() {
+  const navigate = useNavigate()
+
   return (
     <>
       <StateBar />
@@ -11,8 +14,15 @@ function HotelBookingSuccess() {
         <h2>恭喜你完成預訂</h2>
         <h1>訂單代號:HGHT2023032200001</h1>
         <HotelBookingHotel />
-        <button className="hotelSuccessBtn">查詢訂單</button>
-        <button className="hotelSuccessBtn">回飯店首頁</button>
+        <button
+          className="hotelSuccessBtn"
+          onClick={() => navigate(`/hotel//Memberinfo`)}
+        >
+          查詢訂單
+        </button>
+        <button className="hotelSuccessBtn" onClick={() => navigate(`/hotel/`)}>
+          回飯店首頁
+        </button>
       </div>
     </>
   )
